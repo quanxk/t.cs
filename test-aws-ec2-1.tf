870fec113e4b0131e8f4a188b12476aa6887227e
Xiaoke Quan <34225552+quanxk@users.noreply.github.com>
2022-05-11 15:24:43 +0800

    Update test-aws-ec2-1.tf

--- test-aws-ec2-1.tf
+++ test-aws-ec2-1.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "myec2" {
   ami = "ami-0b0af3577fe5e3532"
   instance_type = "t2.micro"
+  vpc_security_group_ids = "sg-05096ef7b268469af"
   tags={
       Name = "coco-test-ec2-rh8-1"
   }
