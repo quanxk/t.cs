e143770117d0c03942c898710b662593034945ca
Accurics <admin@accurics.com>
2022-05-11 15:43:34 +0800

    Auto Remediation By Accurics

--- test-aws-ec2-1.tf
+++ test-aws-ec2-1.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "myec2" {
-  ami = "ami-0b0af3577fe5e3532"
+  ami           = "ami-0b0af3577fe5e3532"
   instance_type = "t2.micro"
-  tags={
-      Name = "coco-test-ec2-rh8-1"
+  tags = {
+    Name = "coco-test-ec2-rh8-1"
   }
+  vpc_security_group_ids = ["<security_group_id>"]
 }
